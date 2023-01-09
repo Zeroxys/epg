e5208a52fdbcc3111b160eafd529f9befaebcc18
Miguel Zavala <miguelzavalac@gmail.com>
2023-01-09 15:30:06 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Claro_epg`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
